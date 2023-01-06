0d205ffbf4317365b35b1dcf2367e86967eb3c27
sigit <warsonosigit9@gmail.com>
2023-01-06 17:19:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Autoplay,
+  Virtual
+} from "swiper/core";
+import "swiper/swiper-bundle.css";
+import "./styles.css";
+import "swiper/css/navigation";
 
-function App() {
+SwiperCore.use([Navigation, Pagination, Autoplay, Virtual]);
+
+export default function App() {
+  // const slides = [];
+
+  // for (let i = 0; i < 12; i++) {
+  //   slides.push(
+  //     <SwiperSlide key={`slide-${i}`} style={{ listStyle: "none" }}>
+  //       <div class="wrapper">
+  //         <div class="card">
+  //           <h3 class="card-title">CARD TITLE</h3>
+  //           <p class="card-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a
+  //             piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+  //           <button class="card-btn">READ MORE</button>
+  //         </div>
+  //       </div>
+  //     </SwiperSlide>
+  //   );
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Swiper
+    slidesPerView={1}
+    spaceBetween={8}
+    navigation={true}
+    pagination={{
+      clickable: true,
+    }}
+    breakpoints={{
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    }}
+    modules={[Pagination]}
+    className="mySwiper"
+    >
+     <SwiperSlide>
+     <div class="wrapper">
+      Slide 1
+      </div>
+      </SwiperSlide>
+        <SwiperSlide>
+        <div class="wrapper">
+      Slide 2
+      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div class="wrapper">
+      Slide 3
+      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div class="wrapper">
+      Slide 4
+      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div class="wrapper">
+      Slide 5
+      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div class="wrapper">
+      Slide 6
+      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div class="wrapper">
+      Slide 7
+      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div class="wrapper">
+      Slide 8
+      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div class="wrapper">
+      Slide 9
+      </div>
+        </SwiperSlide>
+    </Swiper>
   );
 }
-
-export default App;
